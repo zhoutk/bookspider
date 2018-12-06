fd9e4cda87d79390c9934e617de7a7cae824a14b
zhoutk <zhoutk@189.cn>
2018-12-06 21:00:32 +0800

    spider is ok.

--- src/common/globUtils.ts
+++ src/common/globUtils.ts
@@ -1,4 +1,22 @@
+import * as uuid from 'uuid'
+import * as fs from 'fs'
+import * as charset from 'superagent-charset'     //解决乱码的问题
+import * as superagent from 'superagent'
+charset(superagent)
+
 export default class GlobUtils {
+    uuid() {
+        return uuid.v1().split('-')[0]
+    }
+    async spiderData(path: string, encoding?: string) {
+        return await superagent
+        .get(path)
+        .buffer(true)
+        .charset(encoding)
+    }
+    spiderDown(path: string, filename: string) {
+        superagent(path).pipe(fs.createWriteStream(filename))
+    }
     isDev() {
         return G.NODE_ENV !== 'prod'
     }
--- src/inits/global.ts
+++ src/inits/global.ts
@@ -10,7 +10,7 @@ const env = process.env.NODE_ENV || 'dev'            //dev - 开发; prod - 生�
 let GlobVar = {
     PAGESIZE: 10,
     STCODES,
-    ROOT_PATH: `${process.cwd()}${env === 'prod' ? '' : '/dist'}`,
+    ROOT_PATH: `${process.cwd()}${env === 'dev' ? '' : '/dist'}`,
     NODE_ENV: env,
     L: lodash,
     logger: (() => {
@@ -33,6 +33,21 @@ let GlobVar = {
 
 async function globInit() {
     Object.assign(global, { G: GlobVar }, { Promise: Bluebird })
+    Date.prototype['Format'] = function (fmt) { 
+        let o = {
+            'M+': this.getMonth() + 1, 
+            'd+': this.getDate(), 
+            'h+': this.getHours(), 
+            'm+': this.getMinutes(),
+            's+': this.getSeconds(),
+            'q+': Math.floor((this.getMonth() + 3) / 3), 
+            'S': this.getMilliseconds() 
+        }
+        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length))
+        for (let k in o)
+            if (new RegExp('(' + k + ')').test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)))
+        return fmt
+    }
 }
 
 class KoaErr extends Error {
