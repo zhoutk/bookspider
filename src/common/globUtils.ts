fa3492f7052efe0a112aa34fcb545cca2135cbda
zhoutk <zhoutk@189.cn>
2018-12-06 19:07:35 +0800

    init spider.

---
+++ src/common/globUtils.ts
@@ -0,0 +1,24 @@
+export default class GlobUtils {
+    isDev() {
+        return G.NODE_ENV !== 'prod'
+    }
+    isLogin() {
+        return true
+    }
+    arryParse(arr): Array<any>|null {
+        try {
+            if (Array.isArray(arr) || G.L.isNull(arr))
+                return arr
+            else if (typeof arr === 'string') {
+                if (arr.startsWith('['))
+                    arr = JSON.parse(arr)
+                else
+                    arr = arr.split(',')
+            } else 
+                return null
+        } catch (err) {
+            arr = null
+        }
+        return arr
+    }
+}
