5e4279ff225853495ba31b38af513c25ed390af7
zhoutk <zhoutk@189.cn>
2018-12-07 11:18:53 +0800

    batch insert.

--- src/dao/books.ts
+++ src/dao/books.ts
@@ -10,10 +10,35 @@ export default class Books extends BaseDao {
         if (isbn.length > 0) {
             let book = G.CircleQueue.find(isbn)
             if (book === undefined) {
+                let rs = await super.retrieve({isbn})
+                if (rs.status === 200) {
+                    G.logger.debug(`ISBN: ${isbn} in database.`)
+                    book = rs.data[0]
+                }
+            } else {
+                G.logger.debug(`ISBN: ${isbn} in memery.`)
+            }
+            if (book === undefined) {
                 let rs = await new SpiderBooks('books').retrieve({isbn})
                 if (rs.status === 200) {
+                    G.logger.debug(`ISBN: ${isbn} spider real time.`)
                     book = rs.data[0]
-                    G.CircleQueue.push(book)
+                    let data = G.CircleQueue.push(book)
+                    if (data) {
+                        let elements = []
+                        let keys = Object.keys(data)
+                        keys.forEach((isbn) => {
+                            elements.push({
+                                isbn,
+                                book_name: data[isbn].book_name,
+                                author_name: data[isbn].author_name,
+                                publisher: data[isbn].cbs,
+                                publish_day: data[isbn].cb_time,
+                                details_json: JSON.stringify(data[isbn])
+                            })
+                        })
+                        super.insertBatch(this.table, elements)
+                    }
                 }
             }
             return book
